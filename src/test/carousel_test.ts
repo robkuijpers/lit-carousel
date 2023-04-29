406fa0bcace70bce25822859ea8967c9d6e740a9
Rob Kuijpers <rob.kuijpers@rabobank.com>
2023-04-29 17:58:15 +0200

    typescript starter setup

---
+++ src/test/carousel_test.ts
@@ -0,0 +1,62 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import { Carousel } from '../carousel.js';
+
+import { assert, fixture } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+
+suite('carousel', () => {
+  test('is defined', () => {
+    const el = document.createElement('my-element');
+    assert.instanceOf(el, Carousel);
+  });
+
+  test('renders with default values', async () => {
+    const el = await fixture(html`<carousel></carousel>`);
+    assert.shadowDom.equal(
+      el,
+      `
+      <h1>Hello, World!</h1>
+      <button part="button">Click Count: 0</button>
+      <slot></slot>
+    `
+    );
+  });
+
+  test('renders with a set name', async () => {
+    const el = await fixture(html`<carousel name="Test"></carousel>`);
+    assert.shadowDom.equal(
+      el,
+      `
+      <h1>Hello, Test!</h1>
+      <button part="button">Click Count: 0</button>
+      <slot></slot>
+    `
+    );
+  });
+
+  test('handles a click', async () => {
+    const el = (await fixture(html`<carousel></carousel>`)) as Carousel;
+    const button = el.shadowRoot!.querySelector('button')!;
+    button.click();
+    await el.updateComplete;
+    assert.shadowDom.equal(
+      el,
+      `
+      <h1>Hello, World!</h1>
+      <button part="button">Click Count: 1</button>
+      <slot></slot>
+    `
+    );
+  });
+
+  test('styling applied', async () => {
+    const el = (await fixture(html`<carousel></carousel>`)) as Carousel;
+    await el.updateComplete;
+    assert.equal(getComputedStyle(el).paddingTop, '16px');
+  });
+});
